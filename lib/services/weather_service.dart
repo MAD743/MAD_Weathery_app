c8c4c64dd6b32bb9528df62774ee0bf71689962a
Win Nguyen <winnguyen@Wins-MBP.attlocal.net>
2025-04-22 22:12:16 -0400

    updated weather service

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -2,13 +2,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/weather_model.dart';
 
+
 class WeatherService {
   final String apiKey = 'YOUR_API_KEY_HERE';
 
+
   Future<Weather> getWeatherByLocation(double lat, double lon) async {
     final url = Uri.parse(
-      'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=metric&appid=$apiKey',
+      'https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&units=metric&appid=$apiKey',
     );
+
 
     final response = await http.get(url);
     if (response.statusCode == 200) {
